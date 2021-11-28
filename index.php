74e4c8f7d5e4432d4cf661885a0f9e42a77e728b
Samuel Hirst <samuel.hirst@student.manchester.ac.uk>
2021-11-28 16:42:32 +0000

    create home page placeholder

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include_once("components/header.php") ?>
+
+<div id="teaser-wrapper">
+    <h1>Course Unit Reviews</h3>
+    <hr />
+    <h3>Coming Soon</h3>
+</div>
+
+<?php include_once("components/footer.php") ?>
