f85fefbe9ed87e7594204a6f00a89fed31310b9c
Samuel Hirst <samuel.hirst@student.manchester.ac.uk>
2021-12-11 00:40:52 +0100

    create 404 page
    resolves #12

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php include_once("components/header.php") ?>
+
+<div id="not-found-wrapper">
+    <h1>Error 404</h3>
+    <hr />
+    <h3>Page Not Found</h3>
+</div>
+
+<?php include_once("components/footer.php") ?>
