4d89b4b0202795266a8f18d6219547221f2edc27
Samuel Hirst <samuel.hirst@student.manchester.ac.uk>
2021-12-28 22:58:09 +0000

    create landing page
    resolves #1

--- home.php
+++ home.php
@@ -1,9 +1,16 @@
 <?php include_once("components/header.php") ?>
 
-<div id="teaser-wrapper">
-    <h1>Course Unit Reviews</h3>
-    <hr />
-    <h3>Coming Soon</h3>
+<div id="landing-background"></div>
+<div id="landing-overlay"></div>
+<div id="landing-content">
+    <div id="center-wrapper">
+        <h1>Welcome to Course Unit Reviews</h1>
+        <p>This is the best place to find out what other students think of Computer Science course units at The University of Manchester.</p>
+        <div class="btn-group">
+            <a href="submit_review" class="btn btn-primary">Write a New Review</a>
+            <a href="search_reviews" class="btn btn-secondary">Read an Existing Review</a>
+        </div>
+    </div>
 </div>
 
 <?php include_once("components/footer.php") ?>
